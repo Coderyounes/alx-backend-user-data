2be97dd85e9c60884aac014b74f030aba39864ec
Coderyounes <younesanas2021@gmail.com>
2024-05-15 17:37:19 +0100

    try to push the only needed function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,23 +5,6 @@ import re
 import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-
 def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """
