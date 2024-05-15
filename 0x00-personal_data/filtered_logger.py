5f7ffa9ad8629a9e1b18a88f3cfad708fa5a6e64
Coderyounes <younesanas2021@gmail.com>
2024-05-15 16:16:20 +0100

    "Implement filter_datum function in main.py"

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,18 @@
 #!/bin/usr/env python3
+""" function redact PII """
+from typing import List
+import re
 
-import logging
 
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    :param fields:
+    :param redaction:
+    :param message:
+    :param separator:
+    :return: redacted message
+    """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
