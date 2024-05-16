793eb6ab5a0577591d0d46565692beecdfaee036
Coderyounes <younesanas2021@gmail.com>
2024-05-16 11:53:26 +0100

    hash passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" salt & hash passwords function"""
+import bcrypt
+
+
+def hash_password(password) -> bytes:
+    """
+    Basic function hash the password
+    :param password: Plain-Text Password
+    :return: Hashed Password
+    """
+    b = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(b, salt)
