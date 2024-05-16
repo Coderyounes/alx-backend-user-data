75225545e51ce57e5cee3ba17b7c1290f3b0c9de
Coderyounes <younesanas2021@gmail.com>
2024-05-16 10:52:36 +0100

    something Wrong with ALX Checker

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -36,13 +36,18 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     :Logic: create a database connection
     :return: a database Connection
     """
-    mydb = mysql.connector.connect(
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
     )
-    return mydb
+    return connection
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
