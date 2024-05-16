d7b0267a1d976df4d0538b458a9b55072c7e08d5
Coderyounes <younesanas2021@gmail.com>
2024-05-16 07:09:10 +0100

    Task3: create a function 'get_db' to handle database connections

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,8 @@
 from typing import List
 import re
 import logging
-
 import mysql.connector.connection
+import os
 
 
 class RedactingFormatter(logging.Formatter):
@@ -32,11 +32,17 @@ class RedactingFormatter(logging.Formatter):
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    # TODO: Create a Database Connection
-    # TODO: use the ENV variables to Connect to db(username, password, dbname)
-    # PS: use os module to obtain credentials from ENV
-    # PS: use 'mysql-connector-python' if needed
-    pass
+    """
+    :Logic: create a database connection
+    :return: a database Connection
+    """
+    mydb = mysql.connector.connect(
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return mydb
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
