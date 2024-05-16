b9d7dad3720f7269e6d38a5ad1d323089cf2e8be
Coderyounes <younesanas2021@gmail.com>
2024-05-16 12:03:51 +0100

    function check  password hash validité

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,13 @@ def hash_password(password: str) -> bytes:
     b = password.encode('utf-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(b, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    :param hashed_password: a Hashed Password
+    :param password:  a Plain Text Password
+    :return: True or False
+    """
+    user_byte = password.encode('utf-8')
+    return bcrypt.checkpw(user_byte, hashed_password)
