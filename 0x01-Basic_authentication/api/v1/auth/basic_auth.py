6e8a7d810ceeb641c5f9fb63446dc32b91894886
Coderyounes <younesanas2021@gmail.com>
2024-05-22 00:36:28 +0100

    create empty class BasicAuth & handle authtype

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" new class for Basic auth """
+from api.v1.auth.auth import Auth
+import os
+
+
+class BasicAuth(Auth):
+    """
+    empty class for BasicAuth usage
+    """
+    pass
