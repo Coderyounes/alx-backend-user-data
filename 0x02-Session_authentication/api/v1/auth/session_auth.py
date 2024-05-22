2edbba5826a9151b06d2fecdd2b87ef2b1e11fd5
Coderyounes <younesanas2021@gmail.com>
2024-05-22 16:32:06 +0100

    define 'create_session' method that generate uuid & append it to a dict

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Documentation: Class & method to encapsulate all session_auth """
-
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -9,4 +9,15 @@ class SessionAuth(Auth):
     Session Auth Class , will contain some session auth
     method in the near future
     """
-    pass
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        user_id (str): userid string
+        return: dict container uuid & userid
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        data_id = str(uuid.uuid4())
+        self.user_id_by_session_id[data_id] = user_id
+        return data_id
