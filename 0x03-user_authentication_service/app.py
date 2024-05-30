33e2a5ff2cfe581cd72fc32a4158295514faa4d0
Coderyounes <younesanas2021@gmail.com>
2024-05-30 02:00:18 +0100

    define 'users' flask function allow user to register

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 """ Basic Flask App """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route("/", methods=['GET'])
 def index():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """
+    Endpoint where users can signup
+    :return: json message
+    """
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+    try:
+        auth.register_user(email, pwd)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
