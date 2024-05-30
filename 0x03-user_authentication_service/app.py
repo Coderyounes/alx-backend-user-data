5b510370287e74226f5b1c8a396e511ac2e090de
Coderyounes <younesanas2021@gmail.com>
2024-05-30 07:30:28 +0100

    define 'logout' route to destroy session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,5 +45,17 @@ def login():
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """
+    :return: None
+    """
+    session = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
