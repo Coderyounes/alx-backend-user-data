77370d26673ee51e1e1533631043cd5996138230
Coderyounes <younesanas2021@gmail.com>
2024-05-30 02:35:06 +0100

    define 'valid_login' method to validate the users passwords

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ function to hash passwords """
 import bcrypt
-from bcrypt import hashpw
+from bcrypt import hashpw, checkpw
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
@@ -37,3 +37,13 @@ class Auth:
             hash_pass = str(_hash_password(password))
             return db.add_user(email, hash_pass)
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                pwd = password.encode("utf-8")
+                return checkpw(pwd, user.hashed_password)
+        except NoResultFound:
+            return False
+        return False
